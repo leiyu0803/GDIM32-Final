5ee01f4f75a2d51fce795df003fc3ddac80df2aa
leiyu0803 <leiyu0803@qq.com>
2026-03-14 20:30:25 -0700

    玩家跳跃

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
     public float speed = 12f;
+    public Rigidbody rb;
+    private bool isGround = true;
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z​;
-        controller.Move(move * speed * Time.deltaTime);
+        rb.MovePosition(transform.position + move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            rb.AddForce(Vector3.up * 3f, ForceMode.Impulse);
+            isGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
     }
 }
